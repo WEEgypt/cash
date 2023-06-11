289761d1d1d8eaef9277e47649b6940f70c3d131
Mamdouh Khalaf <100497009+mamdou75alaf@users.noreply.github.com>
2023-06-11 18:00:08 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -66,11 +66,10 @@ function Print() {
         " LE";
     doc = window.open("", "_blank");
     doc.document.open();
-    doc.document.write("<html><head><title>Cash Details</title><style>body {font-family: monospace;}</style></head><body onafterprint=window.close()><p>Cash Details:</p>");
+    doc.document.write("<html><head><title>Cash Details</title><style>body {font-family: monospace;}</style></head><body onload=window.print(); onfocus=window.close();><p>Cash Details:</p>");
     doc.document.write(document.getElementById("cashdetails").value.replace(/ /gi, "&nbsp;").replace(/\n/gi, "<br>"));
     doc.document.write("</body></html>");
     doc.document.close();
-	doc.print();
 }
 function closePrintView() {
 	doc.close();
