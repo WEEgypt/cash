3d526d9c3f81b5c899389c6b0fadfb9bbe06272b
Mamdouh Khalaf <100497009+mamdou75alaf@users.noreply.github.com>
2023-06-11 19:28:08 +0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -66,7 +66,7 @@ function Print() {
         " LE";
     doc = window.open("", "_blank");
     doc.document.open();
-    doc.document.write("<html><head><title>Cash Details</title><style>body {font-family: monospace;}</style></head><body onload=window.print(); onfocus=window.close(); onafterprint=window.close();><p>Cash Details:</p>");
+    doc.document.write("<html><head><title>Cash Details</title><style>body {font-family: monospace;}</style></head><body onload=window.print();setTimeout(window.close(),500);><p>Cash Details:</p>");
     doc.document.write(document.getElementById("cashdetails").value.replace(/ /gi, "&nbsp;").replace(/\n/gi, "<br>"));
     doc.document.write("</body></html>");
     doc.document.close();
