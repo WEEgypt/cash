61994e85bdbf1eb87a21f8fb8630683e27b4cd62
Mamdouh Khalaf <100497009+mamdou75alaf@users.noreply.github.com>
2023-06-11 13:26:40 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -71,7 +71,7 @@ function Print() {
     doc.document.write("</body></html>");
     doc.document.close();
     doc.print();
-    doc.onafterprint = () => doc.close();
+    setTimeout(function() { doc.close(); }, 0);
 }
 function Check(input) {
     if (input.value.length > input.maxLength) {
