a4850cb4708901b7951d13c6dc28b73f1ebee98f
Mamdouh Khalaf <100497009+mamdou75alaf@users.noreply.github.com>
2024-09-21 19:33:34 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,41 @@
-
-const addResourcesToCache = async (resources) => {
-  const cache = await caches.open("v1");
-  await cache.addAll(resources);
-};
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    addResourcesToCache([
-		'',
-		'index.html',
-		'icon.png',
-		'manifest.webmanifest',
-		'pulltorefresh.js',
-		'style.css',
-    ]),
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+});
+const cacheName = "CashHelper-v1";
+const appShellFiles = [
+  "",
+  "index.html",
+  "icon.png",
+  "manifest.webmanifest",
+  "pulltorefresh.js",
+  "style.css",
+];
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheName);
+      console.log("[Service Worker] Caching all: app shell and content");
+      await cache.addAll(contentToCache);
+    })(),
+  );
+});
+self.addEventListener("fetch", (e) => {
+  console.log(`[Service Worker] Fetched resource ${e.request.url}`);
+});
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) {
+        return r;
+      }
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })(),
   );
 });
