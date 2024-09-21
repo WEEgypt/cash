6900cdb3ae9baccb734b475cc1d37e4bffe1a674
Mamdouh Khalaf <100497009+mamdou75alaf@users.noreply.github.com>
2024-09-21 18:36:48 +0300

    beta

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-pwa-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/icon.png',
+        '/manifest.webmanifest',
+        '/pulltorefresh.js',
+        '/style.css',
+      ]);
+    })
+  );
+});
