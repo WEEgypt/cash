d967a31140bba83ca3f38cd6b288241b993c8b97
Mamdouh Khalaf <100497009+mamdou75alaf@users.noreply.github.com>
2024-09-22 05:01:25 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,8 +14,8 @@
 }
 self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open("my-pwa-cache").then((cache) => {
-            return cache.addAll(["icon.png", "index.html", "manifest.webmanifest", "pulltorefresh.js", "style.css"]);
+        caches.open("cashHelper-v1").then((cache) => {
+            return cache.addAll(["icon.png", "index.html", "manifest.json", "pulltorefresh.js", "style.css"]);
         })
     );
 });
