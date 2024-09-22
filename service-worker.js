bca2db2e7cc463efe59606e3f58d3f1afb66e1d7
Mamdouh Khalaf <100497009+mamdou75alaf@users.noreply.github.com>
2024-09-22 23:27:52 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -26,3 +26,14 @@ self.addEventListener("fetch", (event) => {
         })
     );
 });
+self.addEventListener('push', function(event) {
+    const data = event.data.json();  // Assuming the server sends JSON
+    const options = {
+        body: data.body,
+        icon: 'icon.png',
+        badge: 'badge.png'
+    };
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
