2a06b54935e8788ace3aef5494525c506d6c5393
Mamdouh Khalaf <100497009+mamdou75alaf@users.noreply.github.com>
2024-09-23 03:19:34 +0300

    test

--- service-worker.js
+++ service-worker.js
@@ -26,3 +26,50 @@ self.addEventListener("fetch", (event) => {
         })
     );
 });
+function registerServiceWorker() {
+  return navigator.serviceWorker
+    .register("service-worker.js")
+    .then(function (registration) {
+      console.log("Service worker successfully registered.");
+      return registration;
+    })
+    .catch(function (err) {
+      console.error("Unable to register service worker.", err);
+    });
+}
+function requestPermission() {
+  return new Promise((resolve, reject) => {
+    const permissionResult = Notification.requestPermission((result) => {
+      resolve(result);
+    });
+    if (permissionResult) {
+      permissionResult.then(resolve, reject);
+    }
+  }).then((permissionResult) => {
+    if (permissionResult !== "granted") {
+      throw new Error("Permission denied");
+    }
+
+    subscribeUserToPush();
+  });
+}
+async function subscribeUserToPush() {
+  const registration = await registerServiceWorker();
+
+  const subscribeOptions = {
+    userVisibleOnly: true,
+    applicationServerKey: PUBLIC_VAPID_KEY,
+  };
+
+  const pushSubscription = await registration.pushManager.subscribe(
+    subscribeOptions
+  );
+
+  axios
+    .post("/api/subscription", pushSubscription)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => console.log(error));
+  return pushSubscription;
+}
