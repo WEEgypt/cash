df5f533e501902c16036fd6abc95aebdb444df31
Mamdouh Khalaf <100497009+mamdou75alaf@users.noreply.github.com>
2024-09-23 11:33:17 +0300

    u

--- main.js
+++ main.js
@@ -48,6 +48,31 @@ function sendSubscriptionToServer(subscription) {
 	console.warn(JSON.stringify(subscription))
 }
 
+const targetUrl = '...';
+
+self.addEventListener('notificationclick', (event) => {
+  self.console.log('notificationclick');
+  event.notification.close(); // Android needs explicit close.
+  event.waitUntil(
+    clients.matchAll({type: 'window'}).then( windowClients => {
+      // Check if there is already a window/tab open with the target URL
+      for (var i = 0; i < windowClients.length; i++) {
+        var client = windowClients[i];
+        // If so, just focus it.
+        if (client.url === targetUrl && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      // If not, then open the target URL in a new window/tab.
+      if (clients.openWindow) {
+        return clients.openWindow(targetUrl);
+      }
+    })
+  );
+});
+
+
+
 window.addEventListener('load', () => {
   initServiceWorker();
 });
